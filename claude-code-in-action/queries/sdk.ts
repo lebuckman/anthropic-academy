d4fee9883e4ccd09ac238b0ed2f162a48b528a99
lebuckman <liambuckman0@gmail.com>
2026-04-04 15:09:50 -0700

    feat: write with claude code sdk

--- claude-code-in-action/queries/sdk.ts
+++ claude-code-in-action/queries/sdk.ts
@@ -2,10 +2,13 @@
 // to "@anthropic-ai/claude-agent-sdk"
 import { query } from "@anthropic-ai/claude-agent-sdk";
 
-const prompt = "";
+const prompt = "Add a description to the package.json file";
 
 for await (const message of query({
   prompt,
+  options: {
+    allowedTools: ["Edit"]
+  }
 })) {
   console.log(JSON.stringify(message, null, 2));
 }
